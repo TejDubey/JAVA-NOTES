2bc01e132a893a7618d86cb5c69e588e34792eb3
TejDubey <dubeytej8@gmail.com>
2025-06-28 19:48:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -264,8 +264,44 @@ class helloworld{
 }
 */
 
+//throw and throwss in java...............
 
-//throw and throwss in java
+//hackerrank
+/*import java.util.Scanner;
+class MyCalculator {
+    long power(int n,int p) throws Exception{
+        
+        if(n==0 && p==0) throw new Exception("n and p should not be zero.");
+        else if(n<0 || p<0) throw new Exception("n or p should not be negative.");
+        
+        int res=1;
+        while(p!=0){
+            res=res*n;
+            p--;
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+*/
+
 /*class Negativer extends Exception{
     @Override
     public String toString(){  //used to give exception name actual
